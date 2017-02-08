19c7caad4cda29ba6e277dffa0ebdfa9ffb7e112
Salmon <selvestr.stal@gmail.com>
2017-02-08 23:36:20 +0300

    slug done

--- backend/views/article/_form.php
+++ backend/views/article/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use dosamigos\tinymce\TinyMce;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Article */
@@ -10,30 +11,78 @@ use yii\widgets\ActiveForm;
 
 <div class="article-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'body')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'category_id')->dropDownList(ArrayHelper::map(\common\models\Category::getCategories(), 'id', 'title')); ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    <?php echo $form->field($model, 'announcement')->widget(TinyMce::className(), [
+        'options' => ['rows' => 6],
+        'language' => 'ru',
+        'clientOptions' => [
+            'plugins' => [
+                'advlist autolink lists link image',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime paste code'
+            ],
+            'menubar' => false,
+            'maxLength' => 10,
+            'toolbar' => 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+        ]
+    ]); ?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?php echo $form->field($model, 'body')->widget(TinyMce::className(), [
+        'options' => ['rows' => 10],
+        'language' => 'ru',
+        'clientOptions' => [
+            'plugins' => [
+                'advlist autolink lists link image',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime paste code'
+            ],
+            'menubar' => false,
+            'toolbar' => 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image'
+        ]
+    ]); ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    <?php echo $form->field($model, 'image')->widget(FileInput::classname(), [
+        'options' => [
+            'accept' => 'image/*',
+        ],
+        'pluginOptions' => !$model->isNewRecord ? [
+            'showPreview' => true,
+            'showCaption' => true,
+            'showRemove' => true,
+            'removeClass' => 'btn btn-danger fileinput-remove article-image-remove-button',
+            'removeIcon' => '<i class="glyphicon glyphicon-trash" model_id="' . $model->id . '"></i>',
+            'showUpload' => false,
+            'mainClass' => 'input-group-sm',
+            'showClose' => false,
+            'initialPreview' => $model->getImagePath(),
+            'initialPreviewAsData' => true,
+            'initialCaption' => $model->id . '.jpg',
+            'overwriteInitial' => true,
+            'initialPreviewConfig' => [
+                'caption' => $model->id . '.jpg',
+            ],
+        ] : []
+    ]); ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?php echo $form->field($model, 'source')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'source')->textInput(['maxlength' => true]) ?>
+    <?php echo $form->field($model, 'created_at')->textInput() ?>
 
-    <?= $form->field($model, 'rating')->textInput() ?>
+    <?php echo $form->field($model, 'updated_at')->textInput() ?>
 
-    <?= $form->field($model, 'views')->textInput() ?>
+    <?php echo $form->field($model, 'rating')->textInput() ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?php echo $form->field($model, 'views')->textInput() ?>
+
+    <?php echo $form->field($model, 'status')->dropDownList(\common\models\Article::getStatuses()); ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?php echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>

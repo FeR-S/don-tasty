492f7613d33b758af8373acf36046ce07d6bb2db
Сельвестр Сталоневич <selvestr.stal@gmail.com>
2017-02-16 10:20:20 +0300

    --

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -81,7 +81,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <script type="text/javascript">
                         window.onload = function () {
                             VK.init({apiId: 5877934, onlyWidgets: true});
-                            VK.Widgets.Comments("articles-comment-<?php echo $model->id; ?>", {limit: 10, width: "665", attach: "*"});
+                            VK.Widgets.Comments("articles-comment-<?php echo $model->id; ?>", {limit: 10, attach: "*"});
                         }
                     </script>
 <!--                    --><?php
